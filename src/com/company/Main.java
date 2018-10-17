528ffe6f2704e0531f324c405ff52cc8c2539bc4
Peter Chen <cingpo@sina.com>
2018-10-17 17:46:16 +0800

    Update Main.java
    add 1 from website

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Hello World");
 	System.out.println("Second Line");
+	System.out.println("add 1 from website");	    
     }
 }
