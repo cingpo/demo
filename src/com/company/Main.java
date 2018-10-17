115f391722699277060426ebbf302599565aaa7a
Peter Chen <cingpo@sina.com>
2018-10-17 17:57:32 +0800

    Add forth line

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,14 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World");
+<<<<<<< HEAD
 	System.out.println("Second Line");
 	//new
 	System.out.println("add 1 from website");
+=======
+	    System.out.println("Second Line");
+	    System.out.println("Third Line");
+        System.out.println("Forth Line");
+>>>>>>> Add forth line
     }
 }
