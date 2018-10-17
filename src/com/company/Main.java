a3970a0a1ec1d313cedb0dfdb25dfa77b0d8c5e3
Peter Chen <cingpo@sina.com>
2018-10-17 17:55:56 +0800

    Add third line
    # Conflicts:
    #	src/com/company/Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World");
-<<<<<<< HEAD
 	System.out.println("Second Line");
-	//new
-	System.out.println("add 1 from website");
-=======
-	    System.out.println("Second Line");
-	    System.out.println("Third Line");
-        System.out.println("Forth Line");
->>>>>>> Add forth line
+	    //add error msg
     }
 }
